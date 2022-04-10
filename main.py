bffe7bb0e5e96796d2f68e03ddbd8985df97cf1c
Veeti <oskari.peltomaa@gmail.com>
2022-04-10 18:48:54 +0300

    added feature to download audio

--- main.py
+++ main.py
@@ -25,11 +25,28 @@ def downloadMP4():
         Label(root, text='There was an error!', font='Arial 10', fg='white', bg='red').place(x=190, y=150)
     video = url.streams.first()
     try:
-        video.download('./downloadedVideos')
+        video.download('./downloads')
         Label(root, text='Video downloaded succesfully!', font='Arial 10', fg='white', bg='green').place(x=160, y=150)
     except:
         Label(root, text='There was an error!', font='Arial 10', fg='white', bg='red').place(x=190, y=150)
 
-Button(bg='white', fg='black', command=downloadMP4, text='Start download', activebackground="red").place(x=200, y=100)
+def downloadMP3():
+    try:
+        url = YouTube(str(link.get()))
+    except:
+        Label(root, text='There was an error!', font='Arial 10', fg='white', bg='red').place(x=190, y=150)
+    try:
+        audio = url.streams.filter(only_audio=True, file_extension='mp4').first()
+    except:
+        Label(root, text='There was an error!', font='Arial 10', fg='white', bg='red').place(x=190, y=150)
+    try:
+        audio.download('./downloads')
+        Label(root, text='Audio downloaded succesfully!', font='Arial 10', fg='white', bg='green').place(x=160, y=150)
+    except:
+        Label(root, text='There was an error!', font='Arial 10', fg='white', bg='red').place(x=190, y=150)
+
+
+Button(bg='white', fg='black', command=downloadMP4, text='Start video download', activebackground="red").place(x=188, y=100)
+Button(bg='white', fg='black', command=downloadMP3, text='Start audio download', activebackground="red").place(x=187, y=125)
 
 root.mainloop()
